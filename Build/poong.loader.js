875980fafcf0a4f99ff6006fddb9083221016564
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2024-11-13 18:51:10 -0500

    Offset paddle vertically from input position

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 52403438,
+    wasmFileSize: 52403475,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
