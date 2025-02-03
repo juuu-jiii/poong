e488c29661ea3bdc8caf83662a663c5bdc22af6d
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-02-02 22:47:58 -0500

    Improve UI visibility in both orientations

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 52405879,
+    wasmFileSize: 52405857,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
