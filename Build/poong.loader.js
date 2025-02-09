6573c78c6143d962343cbcd826a2a9fdf8e305ac
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-02-09 18:27:35 -0500

    Fix combos carrying over from previous games

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 52420903,
+    wasmFileSize: 52420954,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
