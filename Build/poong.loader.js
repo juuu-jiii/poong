8b4141bb6200c4c2ecebad581f60ea4e91b319ed
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-02-19 21:52:06 -0500

    Add instructions screen

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 52428756,
+    wasmFileSize: 52428860,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
