5e240885c808156edd251fcd520ac18955acaf6d
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-03-15 10:25:09 -0700

    Show max trick shot stat on game over screen

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 54716290,
+    wasmFileSize: 54716342,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
