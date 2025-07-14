956deebbbfbd7c5897466485b29084085f6d3ad0
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-07-14 08:48:47 -0400

    Toggle finger input script based on control scheme

--- Build/poong.framework.js
+++ Build/poong.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7121888: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7121943: ($0) => { performance.now = function() { return $0; }; },  
- 7121991: ($0) => { performance.now = function() { return $0; }; },  
- 7122039: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7121904: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7121959: ($0) => { performance.now = function() { return $0; }; },  
+ 7122007: ($0) => { performance.now = function() { return $0; }; },  
+ 7122055: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
