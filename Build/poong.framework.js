ad7e3ffe76632905198e1ca068637ad9752c364c
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-08-09 20:42:16 -0400

    Remove combo timer

--- Build/poong.framework.js
+++ Build/poong.framework.js
@@ -1281,10 +1281,10 @@ function dbg(text) {
 // === Body ===
 
 var ASM_CONSTS = {
-  7120896: () => { Module['emscripten_get_now_backup'] = performance.now; },  
- 7120951: ($0) => { performance.now = function() { return $0; }; },  
- 7120999: ($0) => { performance.now = function() { return $0; }; },  
- 7121047: () => { performance.now = Module['emscripten_get_now_backup']; }
+  7120432: () => { Module['emscripten_get_now_backup'] = performance.now; },  
+ 7120487: ($0) => { performance.now = function() { return $0; }; },  
+ 7120535: ($0) => { performance.now = function() { return $0; }; },  
+ 7120583: () => { performance.now = Module['emscripten_get_now_backup']; }
 };
 
 
