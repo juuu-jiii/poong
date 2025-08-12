7e12f88d5b4c8244b934de7e48a18468d498141d
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-08-12 19:18:13 -0400

    Rename "combo" to "wave bonus"

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 89449120,
+    wasmFileSize: 89450642,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
