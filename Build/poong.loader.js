c3f900ee0b9050a96e0ad873a51b661eb905d3f1
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2025-09-27 16:40:36 -0400

    Correct combo multiplier logic
    I don't know how I let this slide for so long.

--- Build/poong.loader.js
+++ Build/poong.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 104706159,
+    wasmFileSize: 104706149,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
